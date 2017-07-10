c5e3a8bbe5664f3b264cf236e9cbd052ddb0718f
czq <18663104463@163.com>
2017-07-10 17:17:21 +0800

    aa

--- MCV/index.php
+++ MCV/index.php
@@ -1,16 +1,16 @@
 <?php
   header("Content-type:text/html;charset=utf-8");
-  $url = str_replace("/RouteTest/MCV/", '', $_SERVER['REQUEST_URI']);
+  $url = trim(str_replace("/RouteTest/MCV/", '', $_SERVER['REQUEST_URI']), '/');
   $action = explode('/', explode('?', $url)[0]);
-  if ($action[0] == 'new'){
+  if ($action[0] === 'new' && $_SERVER['REQUEST_METHOD'] == 'POST'){
     echo '<h2> [POST]Message is '.$_POST['text'].'</h2>';
   }
-  else if ($action[0] == 'test'){
+  else if ($action[0] === 'test'){
     include('./Controllers/Test.php');
     $obj = new Test;
     $obj -> show();
   }
-  else if ($action[0] == 'message') {
+  else if ($action[0] === 'message' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     echo '<h2> [GET]Message is '.urldecode($action[1]).'</h2>';
   }
   else {
